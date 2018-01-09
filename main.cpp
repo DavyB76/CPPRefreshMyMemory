d95201a89930fae7852aa2864bda5122814941e8
davybureau <davy.bureau@gmail.com>
2018-01-09 10:58:51 +0100

    first tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define BOOST_TEST_MODULE My Test
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(first_test)
+{
+    int i = 1;
+    BOOST_TEST(i);
+    BOOST_TEST(i == 2);
+}
+
+BOOST_AUTO_TEST_CASE(second_test)
+{
+    int i = 2;
+    BOOST_TEST(i);
+    BOOST_TEST(i == 2);
 }
