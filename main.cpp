8d9aadd264276968cf29d12aa7645f1a0d3a94aa
davybureau <davy.bureau@gmail.com>
2018-02-08 22:41:53 +0100

    Remove Kth last node tests + code

--- main.cpp
+++ main.cpp
@@ -261,4 +261,80 @@ BOOST_AUTO_TEST_CASE(should_return_inverted_items_when_a_ten_item_list_is_to_be_
     BOOST_TEST(resultList->next->next->next->next->next->next->next->next->next->data == 10);
 }
 
+void RemoveKthLatestNode(std::shared_ptr<ListNode<int>> inputList, int kthLastElementToRemove)
+{
+    auto TailDetector(inputList->next);
+
+    int index = 1;
+    while(index < kthLastElementToRemove)
+    {
+        TailDetector = TailDetector->next;
+        index++;
+    }
+
+    auto KthLastNodePredessor(inputList);
+
+    while(TailDetector->next != nullptr)
+    {
+        TailDetector = TailDetector->next;
+        KthLastNodePredessor = KthLastNodePredessor->next;
+    }
+
+    KthLastNodePredessor->next = KthLastNodePredessor->next->next;
+}
+
+BOOST_AUTO_TEST_CASE(should_erase_kth_latest_items_when_a_three_item_list_is_processed)
+{
+    auto DummyHead = std::make_shared<ListNode<int>>(0);
+    auto firstNode = std::make_shared<ListNode<int>>((1));
+    auto secondNode = std::make_shared<ListNode<int>>((2));
+    auto thirdNode = std::make_shared<ListNode<int>>((3));
+
+    DummyHead->next = firstNode;
+    firstNode->next = secondNode;
+    secondNode->next = thirdNode;
+
+    RemoveKthLatestNode(DummyHead, 2);
+
+    BOOST_TEST(DummyHead->next->data == 1);
+    BOOST_TEST(DummyHead->next->next->data == 3);
+    BOOST_TEST(DummyHead->next->next->next == nullptr);
+}
+
+BOOST_AUTO_TEST_CASE(should_erase_kth_latest_items_when_a_ten_item_list_is_processed)
+{
+    auto DummyHead = std::make_shared<ListNode<int>>(0);
+    auto firstNode = std::make_shared<ListNode<int>>((1));
+    auto secondNode = std::make_shared<ListNode<int>>((2));
+    auto thirdNode = std::make_shared<ListNode<int>>((3));
+    auto fourthNode = std::make_shared<ListNode<int>>((4));
+    auto fifthNode = std::make_shared<ListNode<int>>((5));
+    auto sixthNode = std::make_shared<ListNode<int>>((6));
+    auto seventhNode = std::make_shared<ListNode<int>>((7));
+    auto eigthNode = std::make_shared<ListNode<int>>((8));
+    auto ninthNode = std::make_shared<ListNode<int>>((9));
+    auto tenthNode = std::make_shared<ListNode<int>>((10));
+    DummyHead->next = firstNode;
+    firstNode->next = secondNode;
+    secondNode->next = thirdNode;
+    thirdNode->next = fourthNode;
+    fourthNode->next = fifthNode;
+    fifthNode->next = sixthNode;
+    sixthNode->next = seventhNode;
+    seventhNode->next = eigthNode;
+    eigthNode->next = ninthNode;
+    ninthNode->next = tenthNode;
+
+    RemoveKthLatestNode(DummyHead, 3);
+
+    BOOST_TEST(DummyHead->next->data == 1);
+    BOOST_TEST(DummyHead->next->next->data == 2);
+    BOOST_TEST(DummyHead->next->next->next->data == 3);
+    BOOST_TEST(DummyHead->next->next->next->next->data == 4);
+    BOOST_TEST(DummyHead->next->next->next->next->next->data == 5);
+    BOOST_TEST(DummyHead->next->next->next->next->next->next->data == 6);
+    BOOST_TEST(DummyHead->next->next->next->next->next->next->next->data == 7);
+    BOOST_TEST(DummyHead->next->next->next->next->next->next->next->next->data == 9);
+    BOOST_TEST(DummyHead->next->next->next->next->next->next->next->next->next->data == 10);
+}
 
